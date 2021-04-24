71776349d169d377f371e9b4a06bac708e0126f3
Miguel Ángel Delgado Grande <miguelangeldelgadog18.cb3@dgeti.sems.gob.mx>
2021-04-24 12:19:25 -0500

    Agregar archivos de proyecto.

---
+++ picker/picker/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace picker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            pickOper.Items.Add("Suma");
+            pickOper.Items.Add("Resta");
+            pickOper.Items.Add("Multiplicación");
+            pickOper.Items.Add("División");
+        }
+
+        private void pickOper_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String op = pickOper.SelectedItem.ToString();
+            int n1 = Int32.Parse(txt1.Text);
+            int n2 = Int32.Parse(txt2.Text);
+            double res = 0;
+
+            if (op == "Suma")
+                res = n1 + n2;
+            else if (op == "Resta")
+                res = n1 - n2;
+            else if (op == "Multiplicación")
+                res = n1 * n2; 
+            else
+                res = n1 / n2;
+
+            lblres.Text = res + "";
+        }
+    }
+}
